db26892a7515475c5921b1032b188dae57d3a40e
Olga Konnova <109019471+o-konnova@users.noreply.github.com>
2024-01-12 13:43:59 +0200

    Commit from TeleportHQ

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core'
+import { Title, Meta } from '@angular/platform-browser'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.css'],
+})
+export class Home {
+  rawfaa0: string = ' '
+  raw2jb8: string = ' '
+  rawxbx6: string = ' '
+  rawd9q5: string = ' '
+  rawlho0: string = ' '
+  rawmzto: string = ' '
+  rawmlsh: string = ' '
+  rawsr53: string = ' '
+  raw1bgy: string = ' '
+  raww70l: string = ' '
+  rawjnq2: string = ' '
+  raw6h2h: string = ' '
+  rawax9x: string = ' '
+  raw9hv3: string = ' '
+  raw8bkw: string = ' '
+  rawn7a4: string = ' '
+  rawl224: string = ' '
+  raw2czl: string = ' '
+  rawgc4b: string = ' '
+  rawdk4d: string = ' '
+  rawz0br: string = ' '
+  constructor(private title: Title, private meta: Meta) {
+    this.title.setTitle('Exalted Conscious Aardvark')
+    this.meta.addTags([
+      {
+        property: 'og:title',
+        content: 'Exalted Conscious Aardvark',
+      },
+    ])
+  }
+}
